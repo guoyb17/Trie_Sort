90330123e61452de1500dd2857db6604a62dbd42
郭元博 <guoyb17@mails.tsinghua.edu.cn>
2019-11-25 15:57:48 +0800

    Basically finished fixing bugs.

--- Trie_Sort/main.cpp
+++ Trie_Sort/main.cpp
@@ -17,50 +17,119 @@ struct Bin {
 	}
 };
 
-void mk_word(unsigned int word, Bin**& ans) {
-	if (ans == nullptr) ans = new Bin*[33];
-	for (int i = 0; i < 32; i++) {
-		ans[i] = new Bin;
-		ans[i]->bin = ((word & (0x1 << (31 - i))) == 0) ? 0 : 1;
+typedef struct {
+	uint32_t addr;
+	uint32_t len;
+	uint32_t if_index;
+	uint32_t nexthop;
+} RoutingTableEntry;
+
+bool operator==(const RoutingTableEntry& a, const RoutingTableEntry& b) {
+	if (a.addr != b.addr) return false;
+	if (a.if_index != b.if_index) return false;
+	if (a.len != b.len) return false;
+	if (a.nexthop != b.nexthop) return false;
+	return true;
+}
+ostream& operator<<(ostream& out, const RoutingTableEntry& opt) {
+	out << "[RoutingTableEntry] addr = 0x" << hex << opt.addr
+		<< ", len = " << dec << opt.len
+		<< ", if_index = " << opt.if_index
+		<< ", nexthop = 0x" << hex << opt.nexthop << '.';
+	return out;
+}
+
+void mk_word(RoutingTableEntry& info, Bin**& ans) { // ans contains both addr and len
+	if (ans == nullptr) ans = new Bin*[info.len + 1];
+	uint8_t ip[4];
+	ip[0] = info.addr & 0xff;
+	ip[1] = (info.addr & 0xff00) >> 8;
+	ip[2] = (info.addr & 0xff0000) >> 16;
+	ip[3] = (info.addr & 0xff000000) >> 24;
+
+	for (uint32_t j = 0; j < info.len; j++) {
+		ans[j] = new Bin;
+		ans[j]->bin = ((ip[j / 8] & (0x1 << (7 - j % 8))) == 0) ? 0 : 1;
+	}
+	ans[info.len] = nullptr;
+}
+
+void mk_word(uint32_t& info, Bin**& ans) {
+	if (ans == nullptr) ans = new Bin*[32 + 1];
+	uint8_t ip[4];
+	ip[0] = info & 0xff;
+	ip[1] = (info & 0xff00) >> 8;
+	ip[2] = (info & 0xff0000) >> 16;
+	ip[3] = (info & 0xff000000) >> 24;
+	for (uint32_t j = 0; j < 32; j++) {
+		ans[j] = new Bin;
+		ans[j]->bin = ((ip[j / 8] & (0x1 << (7 - j % 8))) == 0) ? 0 : 1;
 	}
 	ans[32] = nullptr;
 }
 
 bool operator==(const Bin& a, const Bin& b) { return a.bin == b.bin; }
 
-Trie<Bin, unsigned int> rtable(nullptr);
-
-void insert(unsigned int ipa, unsigned int ipb, unsigned int ipc, unsigned int ipd) {
-	unsigned int value = (ipa << 24) + (ipb << 16) + (ipc << 8) + ipd;
-	cout << "Insert value: " << value << " -> ";
-	Bin** tmp = nullptr;
-	mk_word(value, tmp);
-	for (int i = 0; i < 32; i++) cout << +tmp[i]->bin;
-	cout << endl;
-	rtable.insert(tmp, &value);
-}
-
-bool remove(unsigned int ipa, unsigned int ipb, unsigned int ipc, unsigned int ipd) {
-	unsigned int value = (ipa << 24) + (ipb << 16) + (ipc << 8) + ipd;
-	cout << "Remove value: " << value << " -> ";
-	Bin** tmp = nullptr;
-	mk_word(value, tmp);
-	for (int i = 0; i < 32; i++) cout << +tmp[i]->bin;
-	cout << endl;
-	return rtable.remove(tmp, &value) >= 0;
-}
+Trie<Bin, RoutingTableEntry> rtable(nullptr);
 
 void print() {
 	system("cls");
 	rtable.print(0);
 }
 
+void lookup_pre(uint32_t addr) {
+	cout << "Lookup " << hex << addr << ':' << endl;
+	vector<const RoutingTableEntry*> ans;
+	Bin** tmp_ipt = nullptr;
+	mk_word(addr, tmp_ipt);
+	rtable.lookup_pre(tmp_ipt, ans);
+	if (!ans.empty()) {
+		cout << "Ans:" << endl;
+		for (const RoutingTableEntry* i : ans) {
+			cout << *i << endl;
+		}
+		const RoutingTableEntry value = *ans[ans.size() - 1];
+		cout << "Select: nexthop = " << hex << value.nexthop << ", if_index = " << dec << value.if_index << endl;
+	}
+	else cout << "Ans: <empty>" << endl;
+}
+
 int main() {
 	// TODO: check methods
-	insert(192, 168, 0, 1);
-	insert(192, 168, 0, 2);
-	insert(10, 0, 2, 1);
+	RoutingTableEntry i1, i2;
+	i1.addr = 0x00030201; // 0x00030201, 24, 9, 0x0203a8c0
+	i1.len = 24;
+	i1.if_index = 9;
+	i1.nexthop = 0x0203a8c0;
+	i2.addr = 0x04030201; // 0x04030201, 32, 10, 0x0109a8c0
+	i2.len = 32;
+	i2.if_index = 10;
+	i2.nexthop = 0x0109a8c0;
+	//i3.addr = 0x04040808;
+	//i3.len = 32;
+	//i3.if_index = 2;
+	//i3.nexthop = 0x0200007f;
+
+	Bin** ipt1 = nullptr;
+	mk_word(i1, ipt1);
+	//lookup_pre(i1.addr);
+	rtable.insert(ipt1, &i1);
+	//lookup_pre(i1.addr);
+	Bin** ipt2 = nullptr;
+	mk_word(i2, ipt2);
+	rtable.insert(ipt2, &i2);
+	//lookup_pre(i1.addr);
+	//lookup_pre(i2.addr);
+
+	rtable.remove(ipt2);
+	rtable.remove(ipt1);
 
 	print();
+
+	//Bin** ipt3 = nullptr;
+	//mk_word(i3, ipt3);
+	//rtable.insert(ipt3, &i3);
+	//print();
+
 	return 0;
 }
--- Trie_Sort/trie.h
+++ Trie_Sort/trie.h
@@ -19,17 +19,19 @@
 using namespace std;
 
 // Trie-Node class template
-// Char: dict keyword (e.g. a-zA-Z0-9, 0/1, etc.)
+// Char: dict keyword (e.g. a-zA-Z0-9 for dictionary, 0/1 for IP address, etc.)
 //     Char needs:
-//         method int seq()   - to get sequence of this Char
-//         static member size - to get number of kinds of Char
-//         operator==         - to compare Char sequence
+//     *   method int seq()   - to get sequence of this Char
+//     *   static member size - to get number of kinds of Char
+//     *   operator==         - to compare Char sequence
 // Info: node info, may be use to store the entire word or other things (e.g. route info)
 //     Info needs:
-//         operator==         - to compare whether two Info are identical
-//         method of copy     - to give Info to node
-// [NOTE]  operator<<         - DEBUG only: output stream
-// WARN: the tree does NOT maintain Info!
+//     *   operator==         - to compare whether two Info are identical
+//     *   method of copy     - to give Info to node
+//         operator<<         - DEBUG only: output stream
+// [WARN] The tree does NOT maintain Info!
+//        Make sure Info keeps valid while tree working!
+//        Also make sure memory managed properly outside the tree if necessary!
 
 template <class Char, class Info>
 class Trie {
@@ -52,7 +54,8 @@ public:
 
 	void insert(Char** word, Info* iinfo) { // word needs to end up with nullptr as a sign
 		if (word[0] == nullptr) {
-			info = iinfo; // will overwrite previous info
+			if (info == nullptr) info = new Info;
+			*info = *iinfo; // will overwrite previous info
 			return;
 		}
 		if (next == nullptr) next = new Trie[size()];
@@ -63,27 +66,23 @@ public:
 	 * @return
 	 * 0 - matched and deleted
 	 * 1 - this node is clear
-	 * -1 - not matched due to non-null rinfo with null info
-	 * -2 - not matched due to unequal rinfo and info
+	 * -1 - not matched due to null info
+	 * -2 - NOT IN USE
 	 * -3 - ERROR not matched due to no children
 	 * -4 - recursively not matched
 	*/
-	int remove(Char** word, Info* rinfo = nullptr) { // if rinfo == nullptr, ignore it
-		if (next == nullptr) return -3;
+	int remove(Char** word) {
 		if (word[0] == nullptr) {
-			if (rinfo != nullptr && info != nullptr && *rinfo == *info) {
+			if (info != nullptr) {
+				delete info;
 				info = nullptr;
 				if (next == nullptr) return 1;
 				else return 0;
 			}
-			else if (rinfo == nullptr) {
-				info = nullptr;
-				return 0;
-			}
-			else if (info == nullptr) return -1;
-			else return -2;
+			else return -1;
 		}
-		int ans = next[word[0]->seq()].remove(&word[1], rinfo);
+		if (next == nullptr) return -3;
+		int ans = next[word[0]->seq()].remove(&word[1]);
 		if (ans == 1) {
 			for (int i = 0; i < size(); i++) {
 				if (!next[i].empty()) return 0;
@@ -99,7 +98,7 @@ public:
 
 	/**
 	 * @param ans collect all matched info
-	 *     [NOTE] vec [long ... short] match only when word totally matched
+	 *     [NOTE] ans <- vec [long ... short] match ONLY WHEN word totally matched
 	 * @return true if this call found ans, false if not
 	 */
 	bool lookup(Char** word, vector<const Info*>& ans) {
@@ -120,7 +119,7 @@ public:
 
 	/**
 	 * @param ans collect all prefix-matched info
-	 *     [NOTE] vec [short ... long] match
+	 *     [NOTE] ans <- vec [short ... long] match; NO guarantee for total match!
 	 * @return true if this call found ans, false if not
 	 */
 	bool lookup_pre(Char** word, vector<const Info*>& ans) {
@@ -131,13 +130,12 @@ public:
 			}
 			else return false;
 		}
-		if (next == nullptr) return false;
 		
-		if (info != nullptr) {
-			ans.push_back(info);
-			return true;
-		}
-		return next[word[0]->seq()].lookup_pre(&word[1], ans);
+		bool tflag = true;
+		if (info != nullptr) ans.push_back(info);
+		else tflag = false;
+		if (next == nullptr) return tflag;
+		return next[word[0]->seq()].lookup_pre(&word[1], ans) || tflag;
 	}
 
 	// DEBUG only: needs Info overload operator<<
@@ -148,19 +146,14 @@ public:
 			for (int i = 0; i < indent; i++) cout << "  ";
 			cout << "Info: " << *info << endl;
 		}
-		for (int i = 0; i < indent; i++) cout << "  ";
 		if (next != nullptr) {
-			cout << "Children: " << endl;
 			for (int i = 0; i < size(); i++) {
 				if (!next[i].empty()) {
 					for (int i = 0; i < indent; i++) cout << "  ";
-					cout << "#" << i << endl;
+					cout << "Children #" << i << endl;
 					next[i].print(indent + 1);
 				}
 			}
-		}
-		else {
-			cout << "Children: <empty>" << endl;
 		}
 	}
 };
